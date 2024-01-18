daf0934a0a1969572533495ad81f4a97be0603a2
songqin <980047129@qq.com>
2024-01-18 10:03:50 +0800

    feat:功能实现

---
+++ Unity/Assets/DynamicBone/Particle.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Seino.DynamicBone
+{
+    public class Particle
+    {
+        public Transform m_Transform;
+        public int m_ParentIndex;
+        public int m_ChildCount;
+        public float m_Damping;
+        public float m_Elasticity;
+        public float m_Stiffness;
+        public float m_Inert;
+        public float m_Friction;
+        public float m_Radius;
+        public float m_BoneLength;
+
+        public float3 m_Position;
+        public float3 m_PrevPosition;
+        public float3 m_InitLocalPosition;
+        public quaternion m_InitLocalRotation;
+
+        public float3 m_TransformPosition;
+        public float3 m_TransformLocalPosition;
+        public float4x4 m_TransformLocalToWorldMatrix;
+    }
+}
