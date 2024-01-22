86357a5428f870d70e0742d553eaa047906b521f
songqin <980047129@qq.com>
2024-01-22 14:56:32 +0800

    feat:增加受力曲线

--- Unity/Assets/DynamicBone/Utils/Wind.cs
+++ Unity/Assets/DynamicBone/Utils/Wind.cs
@@ -9,11 +9,13 @@ namespace Seino.DynamicBone
         public Vector3 dir;
         public float force = 0.01f;
         public DynamicBone DynamicBone;
+        public float min = 0f;
+        public float max = 0.003f;
 
         private void Update()
         {
             var dirNormal = dir.normalized;
-            var randomForce = force + Random.Range(-0.01f, 0.01f);
+            var randomForce = force + Random.Range(min, max);
             DynamicBone.m_Force = dirNormal * randomForce;
         }
     }
